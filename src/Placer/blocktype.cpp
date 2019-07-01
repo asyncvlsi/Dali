c912db8f037e35aa45375e539bff9f69f7b405d3
Yang-Yihang <yihangyang1993@gmail.com>
2019-07-01 19:10:13 -0400

    initial commit test

---
+++ src/Placer/blocktype.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yihang Yang on 2019-07-01.
+//
+
+#include "blocktype.hpp"
---
+++ src/Placer/blocktype.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Yihang Yang on 2019-07-01.
+//
+
+#ifndef HPCC_BLOCKTYPE_HPP
+#define HPCC_BLOCKTYPE_HPP
+
+
+class block_type {
+
+};
+
+
+#endif //HPCC_BLOCKTYPE_HPP
