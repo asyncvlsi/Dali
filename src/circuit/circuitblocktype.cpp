bbb5b13a67df6bca64f581841aa5f7f74ea5bec4
Yang-Yihang <yihangyang1993@gmail.com>
2019-07-03 22:10:46 -0400

    update circuit_block_type

--- src/circuit/circuitblocktype.cpp
+++ src/circuit/circuitblocktype.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "circuitblocktype.hpp"
+
+block_type_t::block_type_t(std::string &name, int width, int height) : _name(std::move(name)), _width(width), _height(height) {
+  _num = 0;
+}
+
+std::string block_type_t::name() {
+  return _name;
+}
+
+int block_type_t::num() {
+  return  _num;
+}
+
+int block_type_t::width() {
+  return _width;
+}
+
+int block_type_t::height() {
+  return _height;
+}
--- src/circuit/circuitblocktype.hpp
+++ src/circuit/circuitblocktype.hpp
@@ -2,14 +2,14 @@
 // Created by Yihang Yang on 2019-06-27.
 //
 
-#ifndef HPCC_CIRCUITBLOCKTYPE_HPP
-#define HPCC_CIRCUITBLOCKTYPE_HPP
+#ifndef HPCC_BLOCKTYPE_HPP
+#define HPCC_BLOCKTYPE_HPP
 
 #include <string>
 #include <map>
 #include "circuitpin.hpp"
 
-class blocktype_t {
+class block_type_t {
 private:
   /****essential data entries****/
   std::string _name;
@@ -20,9 +20,14 @@ private:
   /****cached data entries****/
   int _num;
 public:
-  blocktype_t();
+  block_type_t(std::string &name, int width, int height);
+
+  std::string name();
+  int num();
+  int width();
+  int height();
 
 };
 
 
-#endif //HPCC_CIRCUITBLOCKTYPE_HPP
+#endif //HPCC_BLOCKTYPE_HPP
