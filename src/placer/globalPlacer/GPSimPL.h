d2cb1c7757f36bec2de8a5acba016aec697d76cb
Yang-Yihang <yihangyang1993@gmail.com>
2019-08-17 08:02:11 -0400

    update member functions

--- src/placer/globalPlacer/GPSimPL.h
+++ src/placer/globalPlacer/GPSimPL.h
@@ -7,9 +7,13 @@
 
 #include "placer/placer.h"
 #include "circuit/bin.h"
-#include "GPSimPL/simplblockaux.h"
 #include "../../../module/Eigen/Sparse"
 #include "../../../module/Eigen//IterativeLinearSolvers"
+#include "GPSimPL/simplblockaux.h"
+#include "GPSimPL/gridbinindex.h"
+#include "GPSimPL/gridbin.h"
+#include "GPSimPL/boxbin.h"
+#include "GPSimPL/cellcutpoint.h"
 
 typedef Eigen::SparseMatrix<double, Eigen::RowMajor> SpMat; // declares a row-major sparse matrix type of double
 typedef Eigen::Triplet<double> T; // A triplet is a simple object representing a non-zero entry as the triplet: row index, column index, value.
@@ -45,6 +49,35 @@ class GPSimPL: public Placer {
   void UpdateMaxMinCtoCY();
   void BuildProblemB2B(bool is_x_direction, SpMat &A, Eigen::VectorXd &b);
   void QuadraticPlacement();
+
+  void create_grid_bins();
+  void init_update_white_space_LUT();
+  void init_look_ahead_legal();
+  int white_space(GridBinIndex const &ll_index, GridBinIndex const &ur_index);
+  bool write_all_terminal_grid_bins(std::string const &NameOfFile);
+  bool write_not_all_terminal_grid_bins(std::string const &NameOfFile);
+  bool write_overfill_grid_bins(std::string const &NameOfFile);
+  bool write_not_overfill_grid_bins(std::string const &NameOfFile);
+  void update_grid_bins_state();
+  void cluster_sort_grid_bins();
+  bool write_first_n_bin_cluster(std::string const &NameOfFile, size_t n);
+  bool write_first_bin_cluster(std::string const &NameOfFile);
+  bool write_all_bin_cluster(const std::string &NameOfFile);
+  bool write_first_box(std::string const &NameOfFile);
+  bool write_first_box_cell_bounding(std::string const &NameOfFile);
+  void find_box_first_cluster();
+  void box_split(BoxBin &box);
+  void grid_bin_box_split(BoxBin &box);
+  void cell_placement_in_box(BoxBin &box);
+  double cell_overlap(Block *node1, Block *node2);
+  void cell_placement_in_box_molecular_dynamics(BoxBin &box);
+  void cell_placement_in_box_bisection(BoxBin &box);
+  bool recursive_bisection_cell_spreading();
+  void look_ahead_legal();
+  void copy_xy_to_anchor();
+  void swap_xy_anchor();
+  void global_placer();
+  void global_placement ();
 
   void DrawBlockNetList(std::string const &name_of_file= "block_net_list.txt");
   void StartPlacement() override;
