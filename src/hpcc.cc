3cd7832ba01010c7966a5ebf76fc19eb4ddeb0f6
Yang-Yihang <yihangyang1993@gmail.com>
2019-10-15 23:36:56 -0400

    Update hpcc.cc

--- src/hpcc.cc
+++ src/hpcc.cc
@@ -4,13 +4,98 @@
 
 #include <iostream>
 #include "common/global.h"
+#include "circuit.h"
+#include "placer.h"
 
 VerboseLevel globalVerboseLevel = LOG_CRITICAL;
 
 void ReportUsage();
 
 int main(int argc, char *argv[]) {
-  ReportUsage();
+  if (argc < 4) {
+    ReportUsage();
+    return 1;
+  }
+  std::string lef_file_name;
+  std::string def_file_name;
+  std::string str_grid_value_x, str_grid_value_y;
+  std::string str_verbose_level;
+  std::string str_x_grid;
+  std::string str_y_grid;
+
+  for( int i = 1; i < argc; ) {
+    std::string arg(argv[i++]);
+    if (arg == "-lef" && i < argc) {
+      lef_file_name = std::string(argv[i++]);
+    } else if (arg == "-def" && i < argc) {
+      def_file_name = std::string(argv[i++]);
+    } else if (arg == "-grid" && i < argc) {
+      str_x_grid = std::string(argv[i++]);
+      str_y_grid = std::string(argv[i++]);
+    } else if (arg == "-v" && i < argc) {
+      str_verbose_level = std::string(argv[i++]);
+    } else {
+      std::cout << "Unknown option for file reading\n";
+    }
+  }
+
+  if ((lef_file_name.empty())||(def_file_name.empty())) {
+    std::cout << "Invalid input files!\n";
+    ReportUsage();
+    return 1;
+  }
+
+  double x_grid, y_grid;
+  try {
+    x_grid = std::stod(str_x_grid);
+    y_grid = std::stod(str_y_grid);
+  } catch (...) {
+    std::cout << "Invalid input files!\n";
+    ReportUsage();
+    return 1;
+  }
+
+  time_t Time = clock();
+  Circuit circuit;
+  circuit.SetGridValue(x_grid, y_grid);
+  circuit.ReadLefFile(lef_file_name);
+  circuit.ReadDefFile(def_file_name);
+
+  Time = clock() - Time;
+  if (globalVerboseLevel >= LOG_INFO) {
+    std::cout << "File loading complete, time: " << (float) Time / CLOCKS_PER_SEC << std::endl;
+  }
+  circuit.ReportBriefSummary();
+  circuit.ReportHPWL();
+
+  Time = clock();
+  Placer *gb_placer = new GPSimPL;
+  gb_placer->SetInputCircuit(&circuit);
+  gb_placer->SetBoundaryDef();
+  gb_placer->SetFillingRate(1);
+  gb_placer->ReportBoundaries();
+  gb_placer->StartPlacement();
+  gb_placer->GenMATLABScript("gb_result.txt");
+  //gb_placer->SaveNodeTerminal();
+
+  Placer *d_placer = new MDPlacer;
+  d_placer->TakeOver(gb_placer);
+  d_placer->StartPlacement();
+  d_placer->GenMATLABScript("dp_result.txt");
+
+  Placer *legalizer = new TetrisLegalizer;
+  legalizer->TakeOver(gb_placer);
+  legalizer->StartPlacement();
+  legalizer->GenMATLABScript("legalizer_result.txt");
+  //legalizer->SaveDEFFile("circuit.def", def_file);
+
+  delete gb_placer;
+  delete d_placer;
+  delete legalizer;
+
+  Time = clock() - Time;
+  std::cout << "Execution time " << (float)Time/CLOCKS_PER_SEC << "s.\n";
+
   return 0;
 }
 
