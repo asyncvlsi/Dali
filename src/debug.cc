9b4f72229bda030d9fdb4d3c76f78324187745bd
Yang-Yihang <yihangyang1993@gmail.com>
2019-11-17 22:33:18 -0500

    Update debug.cc

--- src/debug.cc
+++ src/debug.cc
@@ -19,8 +19,8 @@ int main() {
   std::string lef_file, def_file;
   lef_file = "../test/out_100K.lef";
   def_file = "../test/out_100K.def";
-  //lef_file = adaptec1_lef;
-  //def_file = adaptec1_def;
+  lef_file = adaptec1_lef;
+  def_file = adaptec1_def;
   time_t Time = clock();
   circuit.ReadLefFile(lef_file);
   circuit.ReadDefFile(def_file);
@@ -48,23 +48,24 @@ int main() {
   gb_placer->SetFillingRate(1);
   gb_placer->ReportBoundaries();
   gb_placer->StartPlacement();
-  gb_placer->GenMATLABScript("gb_result.txt");
-  //gb_placer->SaveNodeTerminal();
+  //gb_placer->GenMATLABScript("gb_result.txt");
+  gb_placer->SaveNodeTerminal();
+  gb_placer->SaveDEFFile("circuit.def", def_file);
 
   /*Placer *d_placer = new MDPlacer;
   d_placer->TakeOver(gb_placer);
   d_placer->StartPlacement();
   d_placer->GenMATLABScript("dp_result.txt");*/
 
-  Placer *legalizer = new TetrisLegalizer;
+  /*Placer *legalizer = new TetrisLegalizer;
   legalizer->TakeOver(gb_placer);
   legalizer->StartPlacement();
-  legalizer->GenMATLABScript("legalizer_result.txt");
+  legalizer->GenMATLABScript("legalizer_result.txt");*/
   //legalizer->SaveDEFFile("circuit.def", def_file);
 
   delete gb_placer;
   //delete d_placer;
-  delete legalizer;
+  //delete legalizer;
 
   Time = clock() - Time;
   std::cout << "Execution time " << (float)Time/CLOCKS_PER_SEC << "s.\n";
