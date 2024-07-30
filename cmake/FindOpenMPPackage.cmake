93756d49c06f4f3c036c44cd1bd75b2022000dd8
Karthi Srinivasan <87223671+karthisrinivasan@users.noreply.github.com>
2024-07-30 17:45:16 -0400

    Update FindOpenMPPackage.cmake

--- cmake/FindOpenMPPackage.cmake
+++ cmake/FindOpenMPPackage.cmake
@@ -2,11 +2,15 @@
 # Find OpenMP package
 ############################################################################
 if(APPLE)
-    execute_process(
-        COMMAND brew --prefix libomp
-        OUTPUT_VARIABLE HOMEBREW_LIBOMP_PREFIX
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
+    if(NOT DEFINED ENV{HOMEBREW_LIBOMP_PREFIX})
+        execute_process(
+            COMMAND brew --prefix libomp
+            OUTPUT_VARIABLE HOMEBREW_LIBOMP_PREFIX
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+    else()
+        set (HOMEBREW_LIBOMP_PREFIX $ENV{HOMEBREW_LIBOMP_PREFIX})
+    endif()
     set(OpenMP_CXX_FLAGS "-Xpreprocessor -I${HOMEBREW_LIBOMP_PREFIX}/include")
     set(OpenMP_CXX_LIB_NAMES omp)
     set(OpenMP_omp_LIBRARY ${HOMEBREW_LIBOMP_PREFIX}/lib/libomp.dylib ${HOMEBREW_LIBOMP_PREFIX}/lib/libomp.a)
